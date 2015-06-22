eb49828476b223665fe5c851ef9c8705bc1eeeeb
quentin <quentin.python@vub.ac.be>
2015-06-22 15:40:27 +0200

    adds few explanations at the beginning of the file. Checks options consistently. Enables the counting of events using a DAS querry.

--- count_events.py
+++ count_events.py
@@ -1,23 +1,25 @@
 # This script will count the number of events per dataset for a list of datasets.
-# Either you already have a list of dataset and you run count_events.py -i <yourfile>. Your file should not contain empty lines!
-# If not a list of dataset will be created by making a DAS querry.
+# There is two distinct use cases
+# i) You already have a list of datasets for which you want to know the number of events of
+# Syntax example: "count_events.py - i my_file.txt"  where my_file.txt is a file without any empty lines
+# ii) You do not have a list of datasets. In that case, a list of datasets that matches a das querry will be created by the script.
+#Syntax example: "count_events.py -q /*TTbar_13/*7_4_3-*/GEN-SIM-RECO"
 
 import sys, getopt
 import os
 import subprocess
 from subprocess import Popen, PIPE, STDOUT
 
-#print "There is no input file, the list of dataset will be created by the das querry..."
 
 # Option to give an list of dataset as argument
 def main(argv):
     inputfile = ''
     try:
-#        opts, args = getopt.getopt(argv,"hi:q",["ifile=","qquerry="])
-        opts, args = getopt.getopt(argv,"hi:",["ifile="])
+        opts, args = getopt.getopt(argv,"hi:q",["ifile=","qquerry="])
     except getopt.GetoptError:
-        print "bad option(s)!! Syntax is ..."
-        print 'count_events.py -i <inputfile>'
+        print "Bad options! Use either ..."
+        print "count_events.py -i <inputfile> or"
+        print "count_events.py -q <querry>"
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
@@ -28,47 +30,51 @@ def main(argv):
             inputfile = arg
             if (inputfile != ""):
                 print "The input file is '",inputfile,"'."
-#        elif opt in ("-q","qquerry"):
-#            querry = arg
-#            print "querry is ", querry
+        elif opt in ("-q","qquerry"):
+            querry = arg
+            print "querry is ", querry
 if __name__ == "__main__":
     main(sys.argv[1:])
     
 
+if (len(sys.argv) == 5):
+    print "too many arguments!"
+    print "Use either ..."
+    print "count_events.py -i <inputfile> or"
+    print "count_events.py -q <querry>"
+    sys.exit(2)
 
 # Setting up
 dbsCmd = "cd /cvmfs/cms.cern.ch/slc5_amd64_gcc462/cms/cmssw/CMSSW_6_0_1/src; eval `scramv1 runtime -sh`;"
 pExe = Popen(dbsCmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
 
 
-# Single dataset example
-#dataset = "/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-scaledown-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM"
-#dbsCmd1 = "das_client.py --query='file dataset=/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-scaledown-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM | grep file.nevents'"
+# create a file if the -q option was used
+if (len(sys.argv) > 1):
+    if (sys.argv[1] == "-q"):
+        querry=sys.argv[2]
+        # Crate a text file with the list of dataset you want to get the number of events of
+        f0 = open('querry_samples.txt', 'w')
+        dbsCmd0 = "das_client.py --query='dataset="+querry+"' --limit=0"
+        print "All the dataset that matches 'das_client.py --query='dataset="+querry+"' --limit=0' will be listed!"
+        fetchsamples = Popen(dbsCmd0, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
 
-
-# create a file only if not provided in the argument
-if (len(sys.argv) == 1):
-    # Crate a text file with the list of dataset you want to get the number of events of
-    f0 = open('DR74_samples.txt', 'w')
-    dbsCmd0 = "das_client.py --query='dataset=/*/*DR74*/MINIAODSIM' --limit=0"
-    print "All the dataset that matches 'das_client.py --query='dataset=/*/*DR74*/MINIAODSIM' --limit=0' will be listed!"
-    fetchsamples = Popen(dbsCmd0, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
-
-    # writte the list of dataset in the txt file
-    for row in fetchsamples.stdout:
-        if not (row.startswith("Showing")):
-            f0.write(row)
+        # writte the list of dataset in the txt file
+        for row in fetchsamples.stdout:
+            if not (row.startswith("Showing")):
+                f0.write(row)
+        f0.close()
     
 
 
 datasets = []
 
 # Text file containing the list of dataset is by default the one produced by the DAS querry
-f = open('DR74_samples.txt', 'r')   
+f = open('querry_samples.txt', 'r')   
 
-# overwritte only if the file was provided in the argument
+# overwritte the default file if a file was given using the -i option
 if (len(sys.argv) > 1):
-    if not (sys.argv[2] == ""):
+    if (sys.argv[1] == "-i"):
         f = open(sys.argv[2], 'r')
         print "overwritting default input file argument!"
 
@@ -79,7 +85,7 @@ for dataset in f:
     # remove empty line (/n)
     dataset_clean = dataset[:-1]
     datasets.append(dataset_clean)
-    dbsCmd1 = "das_client.py --query='file dataset=" + dataset_clean  +   " | grep file.nevents'"
+    dbsCmd1 = "das_client.py --query='file dataset=" + dataset_clean  +   " | grep file.nevents' --limit=0"
     list = Popen(dbsCmd1, shell=True, stdin=PIPE, stdout=PIPE, stderr=STDOUT, close_fds=True)
     
     for line in list.stdout:
@@ -90,6 +96,6 @@ for dataset in f:
         if not (linestr_clean == ""):
             number  = int(linestr_clean)
             numbers.append(number)
-
+#        print numbers
     print "The total number of events in the dataset " + dataset_clean +   " = " + str(sum(numbers))
 
